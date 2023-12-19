c6717224d0673823e490e31fb423884fd0b7aa2e
Divine Edwin <edwindivine5@gmail.com>
2023-12-19 01:02:19 +0100

    fixed button component

--- app/components/button/page.tsx
+++ app/components/button/page.tsx
@@ -1,10 +1,4 @@
-import { FC, ReactNode } from "react";
-
-interface Props {
-  text: string;
-}
-
-const Button: FC<Props> = ({ text }) => {
+const Button = ({ text }: { text: string }) => {
   return (
     <>
       <button className="bg-secondary rounded-3xl text-white font-bold py-2 px-4 text-sm w-full tracking-widest">
@@ -15,4 +9,3 @@ const Button: FC<Props> = ({ text }) => {
 };
 
 export default Button;
-
