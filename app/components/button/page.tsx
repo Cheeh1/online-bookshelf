f46addea4bb61e5e2b95eb5e222ef675ca104a4b
Divine Edwin <edwindivine5@gmail.com>
2023-12-19 06:33:24 +0100

    fix: button component

--- app/components/button/page.tsx
+++ app/components/button/page.tsx
@@ -1,10 +1,30 @@
-const Button = ({ text }: { text: string }) => {
+// const Button = ({ text }: { text: string }) => {
+//   return (
+//     <>
+//       <button className="bg-secondary rounded-3xl text-white font-bold py-2 px-4 text-sm w-full tracking-widest">
+//         {text}
+//       </button>
+//     </>
+//   );
+// };
+
+// export default Button;
+
+import React, { FC, ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  className?: string;
+}
+
+const Button: FC<ButtonProps> = ({ text, className, ...rest }) => {
   return (
-    <>
-      <button className="bg-secondary rounded-3xl text-white font-bold py-2 px-4 text-sm w-full tracking-widest">
-        {text}
-      </button>
-    </>
+    <button
+      className={`bg-secondary rounded-3xl text-white font-bold py-2 px-4 text-sm w-full tracking-widest ${className}`}
+      {...rest}
+    >
+      {text}
+    </button>
   );
 };
 
