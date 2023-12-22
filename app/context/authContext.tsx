8b7384418fa053193a8a8b32f55a79be707dbba9
Divine Edwin <edwindivine5@gmail.com>
2023-12-22 21:13:37 +0100

    fix: firebase not initialize error

--- app/context/authContext.tsx
+++ app/context/authContext.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import {auth} from "@/app/lib/firebase/firebase";
 
 interface AuthContextType {
   user: User | null;
@@ -10,19 +11,19 @@ export const AuthContext = createContext<AuthContextType>({ user: null });
 
 export default function AuthContextProvider ({children}: {children: ReactNode}) {
 
-    const auth = getAuth();
+    // const auth = getAuth();
     const [user, setUser] = useState<User | null>(null)
 
     useEffect(() => {
       const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-        console.log("Auth state changed:", currentUser);
+        // console.log("Auth state changed:", currentUser);
         setUser(currentUser);
       });
 
       return () => {
         unsubscribe();
       };
-    }, [auth]);
+    }, []);
 
 
     const authContextValue: AuthContextType = {
--- app/lib/firebase/firebase.tsx
+++ app/lib/firebase/firebase.tsx
@@ -1,33 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAHo1j1TKxjFSkjHfn0AMEnXm_xW0dXIKE",
-//   authDomain: "online-bookshelf-94741.firebaseapp.com",
-//   projectId: "online-bookshelf-94741",
-//   storageBucket: "online-bookshelf-94741.appspot.com",
-//   messagingSenderId: "646347579401",
-//   appId: "1:646347579401:web:3746fe0b73f263321a58d8",
-//   measurementId: "G-R4D4LBEYLB",
-// };
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig)
+console.log(firebaseConfig);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export {auth};
+
 // const analytics = getAnalytics(app);
